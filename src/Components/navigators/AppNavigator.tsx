7860514de5c54db773765dee20282c0de83674a2
keoni <kimkeonhwi991231@gmail.com>
2023-12-03 16:45:14 +0900

    minsung toss

--- src/Components/navigators/AppNavigator.tsx
+++ src/Components/navigators/AppNavigator.tsx
@@ -1,23 +1,25 @@
 // navigators/AppNavigator.tsx
 
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import IntroScreen from '../../screens/IntroScreen';
-import MainTabNavigator from './TabNavigator';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import IntroScreen from "../../screens/IntroScreen";
+import MainTabNavigator from "./TabNavigator";
 import DetailScreen from "../../screens/DetailScreen";
 import LoginScreen from "../../screens/AuthScreens/LoginScreen";
+
 const PrimaryColor = "#309582";
 const RootStack = createStackNavigator();
 
 function AppNavigator() {
   return (
     <RootStack.Navigator initialRouteName="IntroScreen"
-      screenOptions={{
-        headerBackgroundContainerStyle : {
-          backgroundColor : 'white'
-      }}}
+                         screenOptions={{
+                           headerBackgroundContainerStyle: {
+                             backgroundColor: "white"
+                           }
+                         }}
     >
-      <RootStack.Screen name="IntroScreen" component={IntroScreen} options={{headerShown : false}} />
+      <RootStack.Screen name="IntroScreen" component={IntroScreen} options={{ headerShown: false }} />
       <RootStack.Screen name="MainScreen" component={MainTabNavigator} options={{ headerShown: false }} />
       <RootStack.Screen name="DetailsScreen" component={DetailScreen} options={{ headerShown: false }} />
       <RootStack.Screen name="LoginScreen" component={LoginScreen}
@@ -27,17 +29,18 @@ function AppNavigator() {
                           headerBackTitle: "",
                           headerBackTitleVisible: false, // 뒤로 가기 텍스트 숨기기
                           headerBackTitleStyle: {
-                            color: PrimaryColor,
+                            color: PrimaryColor
                           },
                           headerStyle: {
-                            backgroundColor: 'transparent', // 헤더 배경색 투명하게 설정
-                            borderBottomColor : 'grey',
-                            borderBottomWidth : 1
+                            backgroundColor: "transparent", // 헤더 배경색 투명하게 설정
+                            borderBottomColor: "grey",
+                            borderBottomWidth: 1
                           },
-                          headerTintColor : PrimaryColor,
+                          headerTintColor: PrimaryColor
 
-                        }}  />
+                        }} />
     </RootStack.Navigator>
   );
 }
+
 export default AppNavigator;
